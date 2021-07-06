ab311be202a1c2c0e622fea45c068e6a2cd310b7
Álex Law <junior-302@hotmail.com>
2021-07-05 23:37:37 -0300

    [007] criando controller aluno

--- ProjetcsSchool_Api/Controllers/AlunoController.cs
+++ ProjetcsSchool_Api/Controllers/AlunoController.cs
@@ -1,4 +1,8 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
+using ProjetcsSchool_Api.Data;
+using ProjetcsSchool_Api.models;
+using System.Threading.Tasks;
 
 namespace ProjetcsSchool_Api.Controllers
 {
@@ -6,39 +10,123 @@ namespace ProjetcsSchool_Api.Controllers
     [ApiController]
     public class AlunoController : Controller
     {
-        public AlunoController()
+        public IRepository _repo { get; }
+
+        public AlunoController(IRepository repo)
         {
-            
+            _repo = repo;
         }
 
         [HttpGet]
-        public IActionResult Get()
+        public async Task<IActionResult> get()
         {
-            return Ok();
+            try
+            {
+                var result = await _repo.GetAllAlunosAsync(true);
+                return Ok(result);
+            }
+            catch(System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao conectar com banco de dados");
+            }
         }
 
         [HttpGet("{AlunoId}")]
-        public IActionResult GetById(int AlunoId)
+        public async Task<IActionResult> getById(int AlunoId)
         {
-            return Ok();
+            try
+            {
+                var result = await _repo.GetAlunoAsyncById(AlunoId, true);
+                return Ok(result);
+            }
+            catch(System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao conectar com banco de dados");
+            }
         }
 
-        [HttpPost]
-        public IActionResult Post()
+        [HttpGet("ByProfessor/{ProfessorId}")]
+        public async Task<IActionResult> getByProfessorId(int ProfessorId)
         {
-            return Ok();
+            try
+            {
+                var result = await _repo.GetAlunosAsyncByProfessor(ProfessorId, true);
+                return Ok(result);
+            }
+            catch(System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao conectar com banco de dados");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> post(Aluno model)
+        {
+            try
+            {
+                _repo.Add(model);
+                if (await _repo.SaveChangesAsync()) {
+                    return Created($"/api/aluno/{model.Id}", model);
+                }
+            }
+            catch(System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao conectar com banco de dados");
+            }
+            return BadRequest();
         }
 
         [HttpPut("{AlunoId}")]
-        public IActionResult Put(int AlunoId)
+        public async Task<IActionResult> put(int AlunoId, Aluno model)
         {
-            return Ok();
+            try
+            {
+                var aluno = await _repo.GetAlunoAsyncById(AlunoId, false);
+
+                if (aluno == null) {
+                    return NotFound();
+                }
+
+                _repo.Update(model);
+                if (await _repo.SaveChangesAsync()) {
+                    aluno = await _repo.GetAlunoAsyncById(AlunoId, true);
+                    return Created($"/api/aluno/{model.Id}", aluno);
+                }
+
+            }
+            catch(System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao conectar com banco de dados");
+            }
+
+            return BadRequest();
         }
 
         [HttpDelete("{AlunoId}")]
-        public IActionResult Delete(int AlunoId)
+        public async Task<IActionResult> delete(int AlunoId)
         {
-            return Ok();
+            try
+            {
+                 var aluno = await _repo.GetAlunoAsyncById(AlunoId, false);
+
+                if (aluno == null) {
+                    return NotFound();
+                }
+                
+                 _repo.Delete(aluno);
+
+                if (await _repo.SaveChangesAsync()) {
+                    return Ok();
+                }
+
+            }
+            catch(System.Exception)
+            {
+                return this.StatusCode(StatusCodes.Status500InternalServerError, "Erro ao conectar com banco de dados");
+            }
+
+            return BadRequest();
         }
     }
 }
